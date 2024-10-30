745177143608296eb6646937202ef0afcc4c81b7
Gileno <gileno.cr.duarte@gmail.com>
2024-10-30 17:23:30 -0300

    Alterações nas rotas e  logo.component

--- my-project/src/app/app.routes.ts
+++ my-project/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LogoComponent } from './logo/logo.component';
 
 export const routes: Routes = [
     {
-        path: 'teste',
+        path: '',
         component: LogoComponent,
     }
 ];
