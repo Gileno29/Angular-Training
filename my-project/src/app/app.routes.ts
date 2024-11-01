e418276a198ce80d5318e85fef72cff2da15ecc0
Gileno <gileno.cr.duarte@gmail.com>
2024-11-01 08:54:06 -0300

    Adicionando componente button e passando texto de forma dinamica

--- my-project/src/app/app.routes.ts
+++ my-project/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LogoComponent } from './logo/logo.component';
+import { BotaoComponent } from './botao/botao.component';
 
 export const routes: Routes = [
     {
         path: 'logo',
         component: LogoComponent,
-    }
+    },
+    {
+        path:'button',
+        component:BotaoComponent 
+    },
 ];
---
+++ my-project/src/app/botao/botao.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-botao',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './botao.component.html',
+  styleUrl: './botao.component.css'
+})
+export class BotaoComponent {
+  buttonText: string ='ACESSAR';
+}
